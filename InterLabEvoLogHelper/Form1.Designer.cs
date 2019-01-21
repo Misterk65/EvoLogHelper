7b8313ffcb4b34d479f7d34408d5bd20cd8987b4
Karsten Peucker <karsten.peucker@icloud.com>
2019-01-21 18:01:26 +0900

    First try

--- InterLabEvoLogHelper/Form1.Designer.cs
+++ InterLabEvoLogHelper/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(501, 293);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "Read Logs";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- InterLabEvoLogHelper/Form1.cs
+++ InterLabEvoLogHelper/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,44 @@ namespace InterLabEvoLogHelper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                int i = 0;
+                //
+                // The user selected a folder and pressed the OK button.
+                // We print the number of files found.
+                //
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                //MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                i=files.Length;
+                this.Text = "Files to process: " + i;
+                foreach (var file in files)
+                {
+                    foreach (string line in File.ReadLines(file))
+                    {
+                        string[] arrStrings = line.Split(' ');
+
+                        if (arrStrings.Length >= 4)
+                        {
+                            if ((arrStrings[3].ToLower() != "info"))
+                            {
+                                using (StreamWriter writer = new StreamWriter("d:\\temp\\important.txt",true))
+                                {
+                                    writer.WriteLine(line);
+                                }
+                            }
+                        } 
+                        Application.DoEvents();
+                    }
+                    i--;
+
+                }
+            }
         }
     }
 }
