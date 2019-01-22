7859720ce2a38bc136a0887bda7e9ea7e0a7f1e0
Karsten Peucker <karsten.peucker@icloud.com>
2019-01-22 16:51:30 +0900

    First Beta with Interface

--- InterLabEvoLogHelper/Form1.Designer.cs
+++ InterLabEvoLogHelper/Form1.Designer.cs
@@ -31,15 +31,20 @@
             this.btnRead = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnRead
             // 
-            this.btnRead.Location = new System.Drawing.Point(501, 293);
+            this.btnRead.Location = new System.Drawing.Point(120, 158);
             this.btnRead.Name = "btnRead";
             this.btnRead.Size = new System.Drawing.Size(75, 23);
             this.btnRead.TabIndex = 0;
-            this.btnRead.Text = "Read Logs";
+            this.btnRead.Text = "&Read Logs";
             this.btnRead.UseVisualStyleBackColor = true;
             this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
             // 
@@ -47,15 +52,54 @@
             // 
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(288, 140);
+            this.textBox1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 187);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(312, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(36, 17);
+            this.toolStripStatusLabel1.Text = "None";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Click += new System.EventHandler(this.toolStripProgressBar1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(312, 209);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnRead);
+            this.MaximumSize = new System.Drawing.Size(328, 248);
+            this.MinimumSize = new System.Drawing.Size(328, 248);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -64,6 +108,10 @@
         private System.Windows.Forms.Button btnRead;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
     }
 }
 
--- InterLabEvoLogHelper/Form1.cs
+++ InterLabEvoLogHelper/Form1.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,76 +17,162 @@ namespace InterLabEvoLogHelper
     {
         public string destinationPath;
         public string preamble;
+        public bool abortFunction;
         public Form1()
         {
             InitializeComponent();
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
             this.Text = "Files to process: 0";
+            abortFunction = false;
+            textBox1.ReadOnly = true;
+            textBox1.Text = "";
+            btnRead.Text = "&Read Logs";
+            toolStripStatusLabel1.Text = "None";
         }
 
         private void btnRead_Click(object sender, EventArgs e)
         {
-            DialogResult result = folderBrowserDialog1.ShowDialog();
-            if (result == DialogResult.OK)
+            textBox1.Text = "Consolidation process started" + Environment.NewLine; //Status Notification
+            abortFunction = false;//Reset Variable
+
+            if (btnRead.Text == "&Read Logs")
             {
-                int i = 0;
-                //
-                // The user selected a folder and pressed the OK button.
-                // We print the number of files found.
-                //
-                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
-                i=files.Length;
-                this.Text = "Files to process: " + i;
-
-                // When user clicks button, show the dialog.
-                saveFileDialog1.ShowDialog();
-
-                foreach (var file in files)
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
                 {
-                    i--;
-                    this.Text = "Files to process: " + i;
-                    processFiles(file);
+                    btnRead.Text = "&Abort";
+
+                    int i = 0;
+                    //
+                    // The user selected a folder and pressed the OK button.
+                    // We print the number of files found.
+                    //
+                    textBox1.Text = textBox1.Text + "Selected source path:\n " + folderBrowserDialog1.SelectedPath +
+                                    Environment.NewLine; //Status Notification
+
+                    string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                    i = files.Length;
+                    this.Text = "Files to process: " + i; //Status Notification
+
+                    // When user clicks button, show the dialog.
+                    DialogResult resultSaveDialog = saveFileDialog1.ShowDialog();
+                    if (resultSaveDialog == DialogResult.OK)
+                    {
+                        textBox1.Text = textBox1.Text + "Selected destination path:\n " + saveFileDialog1.FileName +
+                                        Environment.NewLine;//Status Notification
+                        textBox1.Text = textBox1.Text + "Starting consolidation process" + Environment.NewLine;//Status Notification
+
+                        foreach (var file in files)
+                        {
+                            toolStripStatusLabel1.Text = file;
+
+                            i--;
+                            this.Text = "Files to process: " + i;//Status Notification
+
+                            processFiles(file);
+                            Application.DoEvents();
+                        }
+                        
+                        if (!abortFunction)
+                        {
+                            textBox1.Text = textBox1.Text + "Starting sorting process" + Environment.NewLine; //Status Notification
+                            toolStripStatusLabel1.Text = "Now sorting";//Status Notification
+
+                            sortFile(destinationPath);
+
+                            textBox1.Text = textBox1.Text + "Consolidation process finished";//Status Notification
+
+                            CreateChunks();
+
+                            this.Text = "Done!";//Status Notification
+                            toolStripStatusLabel1.Text = "Finished!";//Status Notification
+                            Thread.Sleep(3000);
+                            InitializeControls();
+                        }
+                    }
+                    else
+                    {
+                        btnRead.Text = "&Read Logs";//Reset control
+                        textBox1.Text = "";//Reset control
+
+                        toolStripStatusLabel1.Text = "Process aborted!";//Status Notification 
+                    }
+
                 }
-                sortFile(destinationPath);
-                this.Text = "Done!";
+                else
+                {
+                    textBox1.Text = "";//Reset control
+                    toolStripStatusLabel1.Text = "Process aborted!";//Status Notification
+                    abortFunction = true;
+                }
+                
+            }
+            else
+            {
+                DialogResult abortDialogResult = MessageBox.Show("Do you really want to abort the process?",
+                    "Information", MessageBoxButtons.YesNo);
+                if (abortDialogResult==DialogResult.Yes)
+                {
+                    abortFunction = true;
+                    Application.DoEvents();
+                    textBox1.Text = "";//Reset control
+                    toolStripStatusLabel1.Text = "Process aborted!";//Status Notification
+                    Thread.Sleep(3000);
+                    InitializeControls();
+                }
             }
         }
 
         private void processFiles(string file)
         {
-            string[] origin = file.Split('\\');
-
-            foreach (string line in File.ReadLines(file))
+            if (!abortFunction)
             {
-                string[] arrStrings = line.Split(' ');
+                string[] origin = file.Split('\\');
+                string[] tag = origin[origin.Length - 1].Split('.');
 
-                if (arrStrings.Length >= 4)
+                foreach (string line in File.ReadLines(file))
                 {
-                    if ((arrStrings[3].ToLower() != "info"))
-                    {
-                        //Check arrStrings[0] is Date, if yes, create string arrString[0] ~ [3] plus " - ". If not add string in front
-                        DateTime datetime;
-                        if (DateTime.TryParse(arrStrings[0], out datetime))
-                        {
-                            preamble = arrStrings[0] + " " + arrStrings[1] + " " + arrStrings[2] + " " + arrStrings[3] +
-                                       " - ";
-                            using (StreamWriter writer = new StreamWriter(destinationPath, true))
-                            {
-                                writer.WriteLine(line + " " + origin[origin.Length - 1].Substring(0, origin[origin.Length - 1].Length - 4));
-                            }
-                        }
-                        else
-                        {
-                            using (StreamWriter writer = new StreamWriter(destinationPath, true))
-                            {
-                                writer.WriteLine(preamble + line + " " + origin[origin.Length - 1].Substring(0,origin[origin.Length - 1].Length-4));
-                            }
-                           
-                        }
+                    string newLine = line.TrimStart();//remove leading spaces
 
-                       
+                    string[] arrStrings = newLine.Split(' ');
+
+                 if (arrStrings.Length >= 4)
+                   {
+                        if ((arrStrings[3].ToLower() != "info") && (arrStrings[3].ToLower() != "debug"))
+                        {
+                            //Check arrStrings[0] is Date, if yes, create string arrString[0] ~ [3] plus " - ". If not add string in front
+                            DateTime datetime;
+                            if (DateTime.TryParse(arrStrings[0], out datetime))
+                            {
+                                preamble = arrStrings[0] + " " + arrStrings[1] + " " + arrStrings[2] + " " + arrStrings[3] +
+                                           " - ";
+                                string errorTestErrorString = CreateErrorString(arrStrings);
+                                using (StreamWriter writer = new StreamWriter(destinationPath, true))
+                                {
+                                    writer.WriteLine(preamble + " " + errorTestErrorString + " " + "@@" + tag[0] + "@@");
+                                }
+                            }
+                            else
+                            {
+                                using (StreamWriter writer = new StreamWriter(destinationPath, true))
+                                {
+                                    writer.WriteLine(preamble + "\"" + line + "\"" + " " + "@@" + tag[0] + "@@");
+                                }
+
+                            }
+
+                        }
                     }
-                }
-                Application.DoEvents();
+                    Application.DoEvents();
+               }
+            }
+            else
+            {
+                return;
             }
          
         }
@@ -99,6 +186,7 @@ namespace InterLabEvoLogHelper
 
         private void sortFile(string destinationPath)
         {
+            string[] pathin = destinationPath.Split('.');
             DateTime dateTime;
             string[] lines = File.ReadAllLines(destinationPath);
             var data = lines;
@@ -110,7 +198,89 @@ namespace InterLabEvoLogHelper
                 })
                 .OrderByDescending(x => x.SortKey).ThenByDescending(x => x.SortKeyThenBy)
                 .Select(x => x.Line);
-            File.WriteAllLines(@"E:\temp\sorteddata.csv", sorted);
+
+            textBox1.Text = textBox1.Text + "Destination path for sorted file:\n " + pathin[0] + ".txt" +
+                            Environment.NewLine;//Status Notification
+
+            File.WriteAllLines(pathin[0]+ ".txt", sorted);
+        }
+
+        private string CreateErrorString(string[] lineIn)
+        {
+            string lineOut = "";
+
+            for (int i = 4; i < lineIn.Length; i++)
+            {
+                lineOut = lineOut + lineIn[i] + " ";
+            }
+
+            lineOut = "\"" + lineOut + "\"";
+
+            return lineOut.TrimEnd();
+        }
+
+        private void CreateChunks()
+        {
+            string compare = "";
+            string[] pathin = destinationPath.Split('.');
+            string[] pathChunk = destinationPath.Split('\\');
+            string outputText = "";
+            string outputpath = destinationPath.Replace(pathChunk[pathChunk.Length - 1], "Chunks");
+            string outfile= pathChunk[pathChunk.Length-1].Substring(0, pathChunk[pathChunk.Length - 1].Length - 4);
+
+            if (!Directory.Exists(outputpath))
+            {
+                Directory.CreateDirectory(outputpath);
+                pathChunk = destinationPath.Replace(pathChunk[pathChunk.Length - 1], "Chunks").Split('\\');
+            }
+
+            var lineCount = File.ReadLines(pathin[0] + ".txt").Count();
+
+            toolStripProgressBar1.Maximum = lineCount;
+
+
+            foreach (var line in File.ReadLines(pathin[0] + ".txt"))
+            {
+                string[] toCompare = line.Split(' ');
+
+                if (compare==String.Empty)
+                {
+                    outputText = outputText + line + Environment.NewLine;
+                    compare = toCompare[0];
+                }
+                else if(compare==toCompare[0])
+                {
+                    outputText = outputText + line + Environment.NewLine;
+                    compare = toCompare[0];
+                }
+                else
+                {   destinationPath = outputpath + "\\" +  compare + outfile + ".txt";
+
+                    // This text is always added, making the file longer over time
+                    // if it is not deleted.
+                    using (StreamWriter sw = File.AppendText(destinationPath))
+                    {
+                        sw.Write(outputText);
+                        sw.Close();
+                        outputText = "";
+                        Application.DoEvents();
+                    }
+
+                    compare = toCompare[0];
+                }
+
+                toolStripProgressBar1.Value = lineCount--;
+                toolStripProgressBar1.Text = lineCount.ToString();
+
+                Application.DoEvents();
+            }
+        }
+
+
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
